81972e96f55a08d1f3825edf4256efba5e80fa6f
Irfnd <irfandiabimanyu@gmail.com>
2022-09-06 10:09:46 +0700

    [Update] - Cookie options

--- src/helpers/cookies.js
+++ src/helpers/cookies.js
@@ -4,6 +4,8 @@ import env from "helpers/env";
 const cookiesOptions = {
 	path: "/",
 	maxAge: parse(env.cookieExpires) / 1000,
+	domain: env.modeEnv === "production" ? "masakin-app.vercel.app" : "localhost",
+	sameSite: env.modeEnv === "production" ? "none" : "lax",
 };
 
 export default cookiesOptions;
